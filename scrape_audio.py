bfcd4fbc4f889a8c2ad7be959c8619cb922a19e9
rbcurzon_laptop <curzonronald@gmail.com>
2025-05-08 13:04:50 +0800

    Add scrape_audio.py

---
+++ scrape_audio.py
@@ -0,0 +1,44 @@
+# create a script
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+    import sys
+    import time
+    import requests
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.firefox.options import Options
+
+    if len(sys.argv) != 2:
+        print("Usage: python audio_scrapper.py <URL>")
+        sys.exit(1)
+    
+
+    URL = sys.argv[1]
+
+
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Firefox(options=options)
+    browser.get(URL)
+
+    time.sleep(3)
+
+    rendered_page = browser.page_source
+
+    soup = BeautifulSoup(rendered_page, "html.parser")
+
+    book_chapter_text = soup.find(class_="book-chapter-text").get("title")
+
+    audio_player_src = soup.find("video", class_="audio-player").get("src")
+
+    # output
+    p = Path("")
+    # Download audio
+    audio_page = requests.get(audio_player_src)
+    with open(book_chapter_text.replace(' ', '') + ".mp3", "wb") as file:
+        file.write(audio_page.content)
+
+    browser.quit()
