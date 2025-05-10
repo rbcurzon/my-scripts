65c1525c0192ac681dff948f9466ff3e3a0980c7
rbcurzon_laptop <curzonronald@gmail.com>
2025-05-10 10:52:18 +0800

    Update run_scripts.py

--- run_scripts.py
+++ run_scripts.py
@@ -1,28 +1,18 @@
 import threading
 import subprocess
+from multiprocessing import Pool
+
 import sys
 
 def run_script(script_name, arg1):
     subprocess.run(["python", script_name, arg1])
 
 if __name__ == "__main__":
-
-    
-    first = sys.argv[1]
-    last = sys.argv[2]
-
-    thread_list = []
-    for i in range(1, int(last) + 1, 3):
-        thread = threading.Thread(target=run_script, args=(
-            f"../scrape_audio.py",
-            f"https://live.bible.is/bible/PAGPBS/MRK/{i}?audio_type=audio_drama"
-            ))
-        thread_list.append(thread)
-        thread.start()
-
-    # Wait for all threads to complete
-
-    for thread in thread_list:
-        thread.join()
+    with Pool(processes=2) as pool:
+        script_args = [
+            ("my-scripts/scrape_audio.py", f"https://live.bible.is/bible/ILONGN/MRK/{i}")
+            for i in range(int(sys.argv[1]), int(sys.argv[2]) + 1)
+        ]
+        pool.starmap(run_script, script_args)
 
     print("Scripts have finished executing.")
