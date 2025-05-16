89b50af972a10898064b5214ff3294261ccff377
rbcurzon <curzonronald@gmail.com>
2025-05-16 08:30:42 +0800

    Update push_to_hub.py

--- push_to_hub.py
+++ push_to_hub.py
@@ -4,7 +4,7 @@ from datasets import DatasetDict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Concatenate train directories.')
-    parser.add_argument('input_directory', nargs='+', help='List of input metadata files to concatenate')
+    parser.add_argument('input_directory', help='List of input metadata files to concatenate')
     parser.add_argument('--repo_id', default='username/repo_name', help='Hugging Face Hub repository ID')
     parser.add_argument('--config_name', help='Configuration name for the dataset')
     parser.add_argument('--token', help='Hugging Face Hub token for authentication')
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     ds = DatasetDict()
 
-    ds = load_dataset('audiofolder', data_dir=args.input_directory)
+    ds = DatasetDict.load_from_disk(args.input_directory)
     
     train_test_ds = ds['train'].train_test_split(test_size=0.2, seed=42, shuffle=True)
 
