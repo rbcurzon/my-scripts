b1c12ba2ed2e1bb902ed54407c5b69819303ecf2
rbcurzon <curzonronald@gmail.com>
2025-06-05 09:03:57 +0800

    Update scrape_audio.py

--- scrape_audio.py
+++ scrape_audio.py
@@ -1,48 +1,59 @@
 # create a script
 import sys
 import time
+import logging
+import sys
+from pprint import pprint
+from enum import Enum
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-if __name__ == "__main__":
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+class AudioEnum(str, Enum):
+    """
+    Enum for download audio options.
+    """
+    URL = sys.argv[1]
+    OUTPUT_DIR = str(Path(sys.argv[1]).parent.parent.name)
+    FILE_NAME = str(Path(URL).parent.name) + str(Path(URL).name) + ".mp3"
+
+def main():
     if len(sys.argv) != 2:
         print("Usage: python audio_scrapper.py <URL>")
         sys.exit(1)
     
-
-    URL = sys.argv[1]
-
     options = Options()
     options.add_argument("--headless")
     browser = webdriver.Firefox(options=options)
-    browser.get(URL)
+    browser.get(AudioEnum.URL.value)
 
-    time.sleep(2)
+    time.sleep(3)
 
     rendered_page = browser.page_source
-
     soup = BeautifulSoup(rendered_page, "html.parser")
-
     book_chapter_text = soup.find(class_="book-chapter-text").get("title")
-
     audio_player_src = soup.find("video", class_="audio-player").get("src")
 
-    # print(book_chapter_text)
-    # print(audio_player_src, "\n")
+    logging.info(f"Book Chapter Text: {book_chapter_text}")
+    logging.info(f"Audio Player Source: {audio_player_src}")
 
-    # output
-    p = Path("")
-    # Download audio
     audio_page = requests.get(audio_player_src)
-    output_dir = p / sys.argv[1].split("/")[-3]
+    output_dir = Path(AudioEnum.OUTPUT_DIR.value)
     output_dir.mkdir(parents=True, exist_ok=True)
     
-    with open(output_dir / (book_chapter_text.replace(' ', '') + ".mp3"), "wb") as file:
-        print("Exporting: ",file.name)
+    with open(output_dir / AudioEnum.FILE_NAME.value, "wb") as file:
         file.write(audio_page.content)
+        logging.info(f"Downloaded {file.name} succesfully", )
 
     browser.close()
     browser.quit()
+
+if __name__ == "__main__":
+    main()
