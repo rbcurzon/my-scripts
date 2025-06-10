fbdf08c3370667d57a896774e6ee6039b3415016
rbcurzon_laptop <curzonronald@gmail.com>
2025-06-10 20:24:19 +0800

    Update run_scripts.py

--- run_scripts.py
+++ run_scripts.py
@@ -1,19 +1,19 @@
+import sys
 import threading
 import subprocess
+import multiprocessing as mp
 from multiprocessing import Pool
 
-import sys
-
 def run_script(script_name, arg1):
-    subprocess.run(["python", script_name, arg1])
+    subprocess.run(["python3", script_name, arg1])
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
-        print("Usage: python run_scripts.py <script_name> <output_dir> <num_scripts>")
+        print("Usage: python run_scripts.py <script_name> <url> <num_scripts>")
         sys.exit(1)
 
-    with Pool(processes=2) as pool:
+    with Pool(processes=mp.cpu_count() ) as pool:
         script_args = [
             (sys.argv[1],f"{sys.argv[2]}/{i}")
             for i in range(1, int(sys.argv[3]) + 1)
